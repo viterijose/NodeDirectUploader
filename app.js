da7e5077b3d4700194cc1c7902eb82db36f86ef4
Jose Viteri <Jose@Joses-MBP.home>
2018-06-07 14:37:47 -0400

    test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const S3_BUCKET = process.env.S3_BUCKET;
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
-app.get('/account', (req, res) => res.render('account.html'));
+app.get('/views', (req, res) => res.render('account.html'));
 
 /*
  * Respond to GET requests to /sign-s3.
